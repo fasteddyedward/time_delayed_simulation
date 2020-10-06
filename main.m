69f4f94866b9bd0625383b53d99797c090bb7510
fasteddyedward <61181403+fasteddyedward@users.noreply.github.com>
2020-10-06 17:58:43 +0200

    T=0, v_0=10^-6
    This file here tests the maximum dt (still in progress) for the deterministic case. Note that the v_0 has been changed to 10^6 mm/ms according to Viktor's suggestion.

--- main.m
+++ main.m
@@ -1,4 +1,6 @@
 %% This file runs modulized_time_delay_proto
+%% 2020.10.6 This is a branch for testing deterministic motion of particles (T=0)
+%% Also note that the v_0 has been modified to 10^-6 mm/ms
 clear;
 close all
 %% Setup for Running the program
@@ -9,9 +11,9 @@ Obs_time_steps=10^5
 % Obs_time=Obs_time_steps*dt;
 
 %% Coefficients and parameters
-v_0= 10^-4; % mm/ms
+v_0= 10^-6; % mm/ms
 % gamma=6*pi*1.0016*10^-3*10^-6; % Stoke's drag, gamma=6*pi*eta*a
-T=300; % Kelvin 
+T=30; % Kelvin 
 % k_B=10^-23; % Boltzmann constant
 % D=k_B*T/gamma; % Diffusitivity
 
@@ -21,10 +23,10 @@ for i=1:N
     y_init(i)=0;
 end
 for i=1:N
-    x_init(i)=i*10^-4;
-    y_init(i)=i*10^-4;
+    x_init(i)=i*10^-6;
+    y_init(i)=i*10^-6;
 end
-y_init(3)=2*10^-4;
+y_init(3)=1*10^-6;
 %% Start calculating finite element numericals for the equation of motion
 [x,y,F_x,F_y,v_x,v_y,delta_x,delta_y,time]=modulized_time_delay_proto(N,delta_t,dt,Obs_time_steps,v_0,T,x_init,y_init);
 %% Parameters for making the movies
--- modulized_time_delay_proto.m
+++ modulized_time_delay_proto.m
@@ -21,7 +21,7 @@ D=k_B*T/gamma; % Diffusitivity
 % magnify=1 control_animation_interval=10^4 movie_create='on' ghost='off'
 % axis_choice='lab'; %'cm' or 'lab'
 %% Checking comparison of Driving force and diffusion
-% delta_x=F_x*dt+normrnd(0,sqrt(4*D*dt)) ~ v_0*dt+sqrt(4*D*dt)
+% delta_x=F_x*dt+normrnd(0,sqrt(2*D*dt)) ~ v_0*dt+sqrt(2*D*dt)
 ['The ratio v_0*dt/sqrt(4*D*dt) is ',num2str(v_0*dt/(sqrt(4*D*dt)))]
 %% Warning for choosing time steps 
 %%% If the Obs_time is smaller than time delay delta_t, the F_x and F_y we
@@ -44,6 +44,8 @@ delta_x(1:N,1:Obs_time_steps+delta_t/dt)=0;
 delta_y(1:N,1:Obs_time_steps+delta_t/dt)=0;
 for k=1:1+delta_t/dt
     for i=1:N
+%             delta_x(i,k)=normrnd(0,sqrt(2*D*dt));
+%             delta_y(i,k)=normrnd(0,sqrt(2*D*dt));
             delta_x(i,k)=normrnd(0,sqrt(4*D*dt));
             delta_y(i,k)=normrnd(0,sqrt(4*D*dt));
             x(i,k+1)=x(i,k)+delta_x(i,k);
@@ -58,40 +60,48 @@ for k=1:Obs_time_steps
     diff_x(1:N,1:N)=0;
     diff_y(1:N,1:N)=0;
     %% Particle Interaction: calculating the F_x and F_y with position at delayed times
-    for i=1:N
-        for j=1:N
-            if(j~=i)
-                diff_x(i,j)=x(j,k)-x(i,k);
-                diff_y(i,j)=y(j,k)-y(i,k);
-                if diff_x(i,j)^2+diff_y(i,j)^2==0 && diff_x(i,j)==0 
-                    e_x(i)=e_x(i)+0;
-                else
-                e_x(i)=e_x(i)+diff_x(i,j)/sqrt(diff_x(i,j)^2+diff_y(i,j)^2);
-                end
-                if diff_x(i,j)^2+diff_y(i,j)^2==0 && diff_y(i,j)==0 
-                    e_y(i)=e_y(i)+0 ;
-                else
-                e_y(i)=e_y(i)+diff_y(i,j)/sqrt(diff_x(i,j)^2+diff_y(i,j)^2);
+%     if v_0==0 %% Here I simplify the calculation for pure diffusion v_0=0 to save time
+%         F_x(i,k+delta_t/dt)=0;
+%         F_y(i,k+delta_t/dt)=0;
+%     else
+        for i=1:N
+            for j=1:N
+                if(j~=i)
+                    diff_x(i,j)=x(j,k)-x(i,k);
+                    diff_y(i,j)=y(j,k)-y(i,k);
+                    if diff_x(i,j)^2+diff_y(i,j)^2==0 && diff_x(i,j)==0
+                        e_x(i)=e_x(i)+0;
+                    else
+                        e_x(i)=e_x(i)+diff_x(i,j)/sqrt(diff_x(i,j)^2+diff_y(i,j)^2);
+                    end
+                    if diff_x(i,j)^2+diff_y(i,j)^2==0 && diff_y(i,j)==0
+                        e_y(i)=e_y(i)+0 ;
+                    else
+                        e_y(i)=e_y(i)+diff_y(i,j)/sqrt(diff_x(i,j)^2+diff_y(i,j)^2);
+                    end
                 end
             end
-        end
-        %% Singulartiy
-        %%% If e_x or e_y is 0, F_x & F_y will not be calculated as 0
-        %%% numerically, so we have to put it by hand.
-        F_x(i,k+delta_t/dt)=e_x(i)/sqrt(e_x(i)^2+e_y(i)^2)*v_0;
-        F_y(i,k+delta_t/dt)=e_y(i)/sqrt(e_x(i)^2+e_y(i)^2)*v_0;
-        if e_x(i)^2+e_y(i)^2==0 && e_x(i)==0             
-            F_x(i,k+delta_t/dt)=0;        end
-        if e_x(i)^2+e_y(i)^2==0 && e_y(i)==0             
-            F_y(i,k+delta_t/dt)=0;        end
+            %% Singulartiy
+            %%% If e_x or e_y is 0, F_x & F_y will not be calculated as 0
+            %%% numerically, so we have to put it by hand.
+            F_x(i,k+delta_t/dt)=e_x(i)/sqrt(e_x(i)^2+e_y(i)^2)*v_0;
+            F_y(i,k+delta_t/dt)=e_y(i)/sqrt(e_x(i)^2+e_y(i)^2)*v_0;
+            if e_x(i)^2+e_y(i)^2==0 && e_x(i)==0
+                F_x(i,k+delta_t/dt)=0;        end
+            if e_x(i)^2+e_y(i)^2==0 && e_y(i)==0
+                F_y(i,k+delta_t/dt)=0;        end
+%     end
         %% Storing the particle's displacement (including the fluctuation)
+%         delta_x(i,k+delta_t/dt)=F_x(i,k+delta_t/dt)*dt+normrnd(0,sqrt(2*D*dt));
+%         delta_y(i,k+delta_t/dt)=F_y(i,k+delta_t/dt)*dt+normrnd(0,sqrt(2*D*dt));
         delta_x(i,k+delta_t/dt)=F_x(i,k+delta_t/dt)*dt+normrnd(0,sqrt(4*D*dt));
         delta_y(i,k+delta_t/dt)=F_y(i,k+delta_t/dt)*dt+normrnd(0,sqrt(4*D*dt));
         %% Updating particle position with Particle Interaction and Diffusion
         x(i,1+k+delta_t/dt)=x(i,k+delta_t/dt)+delta_x(i,k+delta_t/dt);
         y(i,1+k+delta_t/dt)=y(i,k+delta_t/dt)+delta_y(i,k+delta_t/dt);
+        end
     end
-end
+% end
 
 %% Calculating the velocities at each timestep (from t=0+delta_t ~ Obs_time+delta_t)
 v_x=delta_x/dt;
