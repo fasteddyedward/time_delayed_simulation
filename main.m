cb91bc388f1564220614216500efa6f9695b2aaa
fasteddyedward <61181403+fasteddyedward@users.noreply.github.com>
2020-10-16 13:48:01 +0200

    Start speeding up; delete F_x and delta_x
    The F_x, F_y, delta_x, delta_y calculated in the modulized_time_delay_proto.m function is not exported to main.m
    Also the saving and loading of files are modified to avoid memory overflow.

--- main.m
+++ main.m
@@ -1,16 +1,27 @@
 %% This file runs modulized_time_delay_proto
 %% 2020.10.14 to make the videos with several tries
-for nth_take=12
-tic
+for nth_take=22
 clearvars -except nth_take
 close all
 %% Output File Name
-movie_name=['2020.10.15,dt=10e-3 take ',num2str(nth_take)]
+% warning('Have you modified the file name?')
+% pause
+movie_name=['2020.10.16,dt=10e-3 take ',num2str(nth_take)];
+answer = questdlg(['Is the file name correct? ',movie_name], ...
+	'File Name Check', ...
+	'Yes','No, I will  modify it','No, I will modify it ');
+switch answer
+    case 'Yes'
+        'Start running program...'
+    case 'No, I will  modify it'
+        error('Please modify the file name')
+end
+tic
 %% Setup for Running the program
 N=3; % number of particles in the play
 delta_t=50; % ms
 dt=10^-3; % ms 
-Obs_time_steps=10^6
+Obs_time_steps=10^7
 % Obs_time=Obs_time_steps*dt;
 
 %% Coefficients and parameters
@@ -31,36 +42,38 @@ for i=1:N
 end
 y_init(3)=2*10^-4;
 %% Start calculating finite element numericals for the equation of motion
-[x,y,F_x,F_y,v_x,v_y,delta_x,delta_y,time]=modulized_time_delay_proto(N,delta_t,dt,Obs_time_steps,v_0,T,x_init,y_init);
+[x,y,~,~,v_x,v_y,~,~,time]=modulized_time_delay_proto(N,delta_t,dt,Obs_time_steps,v_0,T,x_init,y_init);
 save([movie_name,'.mat']);
-clear F_x F_y delta_x delta_y time
+toc
+tic
+clear time
 %% Parameters for making the movies
-magnify=1000
-control_animation_interval=10^4 % Record one frame in every ____ frame
-movie_create='on'
-ghost='on'
+magnify=1000    ;
+control_animation_interval=10^4     ; % Record one frame in every ____ frame
+movie_create='on'   ;
+ghost='on'      ;
 axis_choice='lab'; %'cm' or 'lab'
-leave_trace='off'
+leave_trace='off'       ;
 close all
-(Obs_time_steps+delta_t/dt)/control_animation_interval
+(Obs_time_steps+delta_t/dt)/control_animation_interval      ;
 if ((Obs_time_steps+delta_t/dt)/control_animation_interval>100)
     warning(['The plotting will take about ',num2str((Obs_time_steps+delta_t/dt)/control_animation_interval*0.15),' seconds. For faster time please choose different control_animation_interval'])
 end
 % 1000 takes about 150 seconds. 
 
 %% Start plotting the movies and plots
-load([movie_name,'.mat'],'time')
+load([movie_name,'.mat'],'time','x','y','v_x','v_y')
 [MovieVector,v_omega]=make_movies_plots(N,delta_t,v_0,dt,Obs_time_steps,x,y,NaN,NaN,v_x,v_y,NaN,NaN,time,magnify,control_animation_interval,movie_create,ghost,axis_choice,leave_trace);
-clear time
+clear x y v_x v_y time
 %% Save movie 
 switch movie_create
 case 'on'
-    frame_rate=5
+    frame_rate=5    ;
     save_movie(MovieVector(2:end),movie_name,frame_rate);
 end
 %% Saving work space
-
-load([movie_name,'.mat'],'F_x','F_y','delta_x','delta_y','time')
-save([movie_name,'.mat'])
+save([movie_name,'.mat'],'MovieVector','v_omega','magnify','control_animation_interval','movie_create','ghost','axis_choice','leave_trace','-append')
 toc
+%%
+clear MovieVector v_omega
 end
