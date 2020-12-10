f95fc371b36a9366e95331402fd44fdaf7f8bca9
fasteddyedward <eddyedward0427@gmail.com>
2020-12-10 12:38:24 +0100

    Update Find_Effective_Temp.m

--- Find_Effective_Temp.m
+++ Find_Effective_Temp.m
@@ -3,13 +3,13 @@ close all;
 
 
 %%
-
-Date='2020.11.25'
-nth_take=1
-delta_t_matrix=[0.5:0.5:16]
+Date='2020.11.24'
+nth_take=73
+delta_t_matrix=2
 T_matrix=[1]
-v_0_matrix=2.5
+v_0_matrix=[10.1:0.1:20]
 dt=10^-2
+
 
 
 %%
@@ -20,6 +20,8 @@ Delta_t_matrix=delta_t_matrix;
 %%
 sigma_matrix=[];
 mu_matrix=[];
+T_eff_matrix=[];
+D_eff_matrix=[];
 for delta_t_index=1:length(Delta_t_matrix)
     for T_index=1:length(T_matrix)
         for v_0_index=1:length(V_0_matrix)
@@ -32,7 +34,7 @@ movie_name=[Date,',dt=',num2str(dt),' take ',num2str(nth_take),', T=',num2str(T_
 load([movie_name,'.mat'])
 
 %% Finding the sigma of the omega (effective temperature)
-f=figure(1),clf
+f=figure(1),clf;
 switch plot_hist_fit
     case 'no'
         f.Visible='off';
@@ -46,7 +48,14 @@ x=h.BinEdges(1:end-1)+0.5*h.BinWidth;
 mu=fitresult.b1;
 sigma=fitresult.c1/sqrt(2);
 
+%% Calculating the D_eff and T_eff
+D_eff=sigma^2/(4*dt);
+T_eff=(v_0/(2*a)*delta_t^2*sigma^2)/(8*k_B*dt);
+
+save([movie_name,'.mat'],'D_eff','T_eff','-append')
 %% Appending the matrices
+D_eff_matrix=[D_eff_matrix D_eff];
+T_eff_matrix=[T_eff_matrix T_eff];
 sigma_matrix=[sigma_matrix sigma];
 mu_matrix=[mu_matrix mu];
 nth_take
@@ -76,10 +85,22 @@ ylabel('\mu')
 
 figure(4)
 plot(delta_t_matrix.^2,v_0_matrix*delta_t_matrix.^2.*sigma_matrix.^2,'o')
-title(['v_0*\delta t^2*\sigma^2 vs v_0 t, v_0= ',num2str(v_0),', T=',num2str(T)])
+title(['v_0*\delta t^2*\sigma^2 vs \delta t, v_0= ',num2str(v_0),', T=',num2str(T)])
 % title('Gaussian \sigma vs v_0')
 xlabel('\delta_t^2')
 ylabel('v_0*\delta t^2*\sigma^2 ')
+
+figure(5)
+plot(delta_t_matrix,T_eff_matrix,'o')
+title(['T_{eff} vs \delta t, v_0= ',num2str(v_0),', T=',num2str(T)])
+xlabel('\delta t')
+ylabel('T_{eff}')
+
+figure(6)
+plot(delta_t_matrix,D_eff_matrix,'o')
+title(['D_{eff} vs \delta t, v_0= ',num2str(v_0),', T=',num2str(T)])
+xlabel('\delta t')
+ylabel('D_{eff}')
 end
 %% For v_0_matrix
 if length(v_0_matrix)>1
@@ -103,7 +124,18 @@ title(['v_0*\delta t^2*\sigma^2 vs v_0 t, \delta t= ',num2str(delta_t),', T=',nu
 % title('Gaussian \sigma vs v_0')
 xlabel('v_0')
 ylabel('v_0*\delta t^2*\sigma^2 ')
-end
 
+figure(5)
+plot(v_0_matrix,T_eff_matrix,'o')
+title(['T_{eff} vs v_0, \delta t= ',num2str(delta_t),', T=',num2str(T)])
+xlabel('v_0')
+ylabel('T_{eff}')
+
+figure(6)
+plot(v_0_matrix,D_eff_matrix,'o')
+title(['D_{eff} vs v_0, \delta t= ',num2str(delta_t),', T=',num2str(T)])
+xlabel('v_0')
+ylabel('D_{eff}')
+end
 
 
